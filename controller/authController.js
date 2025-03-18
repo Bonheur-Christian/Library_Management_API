b3f15b76478c17814b06b0ef97dc4f59ab89595a
M.Bonheur <mbonheurc23@gmail.com>
2025-03-18 10:47:32 +0200

    few changes

--- controller/authController.js
+++ controller/authController.js
@@ -8,19 +8,30 @@ module.exports = {
 
         try {
 
-            const user =await UserModel.getuserByEmail(email); 
-            console.log(user);
-                       
+            const user = await UserModel.getuserByEmail(email);
 
-            if(!user)
-                return res.status(404).json({messageError:"Invalid Credentials"});
-            
-            
-            const isMatch =await bcrypt.compare(password, );
+            if (!user)
+                return res.status(404).json({ messageError: "Invalid Credentials" });
 
+
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (!isMatch)
+                return res.status(400).json({ messageError: "Invalid Password " });
+
+
+            req.session.user = {
+                id: user.userID,
+                email: user.email,
+                username: user.username
+            }
+
+            return res.status(200).json({ message: "User logged in", user: req.session.user })
 
 
         } catch (err) {
+            console.log(err);
+            
             return res.status(500).json({ messageError: "Error in user login", Error: err });
         }
     }
