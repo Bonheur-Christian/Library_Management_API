1a84db61323f851a0db84c34f1bb666a33284a6d
M.Bonheur <mbonheurc23@gmail.com>
2025-03-29 21:35:35 +0200

    updated codes

--- view/src/app/signin/page.tsx
+++ view/src/app/signin/page.tsx
@@ -1,22 +1,16 @@
-export default function Home() {
+export default function Signin() {
   return (
     <div className="flex">
       <div className="w-[40%] bg-blue-500 h-screen">
-        <h1 className="text-4xl font-medium text-white text-center w-[80%] pt-[25rem] mx-auto">
-          Welcome To Library Management System!
+        <h1 className="text-4xl font-medium text-white text-center w-[80%] pt-[25rem] mx-auto leading-relaxed">
+          Welcome Back To Library Management System!
         </h1>
       </div>
 
-      <div className="w-[60%] px-32 pt-32 space-y-12 ">
-        <h1 className="text-4xl font-medium text-center">Create Account</h1>
+      <div className="w-[60%] px-32 pt-40 space-y-12 ">
+        <h1 className="text-4xl font-medium text-center">Log Into Your Account</h1>
         <div className="space-y-12 flex flex-col w-[60%] justify-center mx-auto">
-          <input
-            type="text"
-            name="username"
-            placeholder="enter username"
-            className="outline-2 outline-gray-200 bg-gray-100 py-4 px-6 rounded-lg text-gray-700"
-            autoFocus
-          />
+      
           <input
             type="email"
             placeholder="enter email"
@@ -31,17 +25,17 @@ export default function Home() {
           />
 
           <button className="text-white text-xl bg-blue-500 hover:bg-blue-800  duration-500 font-medium w-1/2 justify-center mx-auto py-6 rounded-xl ">
-            Register
+            Login
           </button>
         </div>
 
         <p className="text-xl text-center pt-20">
-          already have an account{" "}
+          you have no account ?{" "}
           <a
-            href="/signin"
+            href="/"
             className="text-blue-500 hover:underline duration-500"
           >
-            enter
+            create one
           </a>
         </p>
       </div>
