2af1676dee5866d08cde86ae6e416317756c9821
M.Bonheur <mbonheurc23@gmail.com>
2025-04-06 14:37:21 +0200

    few updates

--- model/NovelModel.js
+++ model/NovelModel.js
@@ -4,9 +4,9 @@ require("dotenv").config();
 const NovelModel = {
 
     insertBookInStock: async (title, author, isbn, price, published_year, quantity) => {
-        const query = "INSERT INTO library_stock(bookName, book_author , bookISBN, price , published_year, quantity) VALUE(?,?,?,?,?,?)";
+        const query = "INSERT INTO novels(bookName, book_author , bookISBN, price , published_year, quantity) VALUE(?,?,?,?,?,?)";
         try {
-            const [similarBook] = await connection.execute("SELECT * FROM library_stock WHERE bookISBN = ?", [isbn]);
+            const [similarBook] = await connection.execute("SELECT * FROM novels WHERE bookISBN = ?", [isbn]);
 
             if (similarBook.length > 0) {
                 return { error: "Book already exists" };
@@ -39,7 +39,7 @@ const NovelModel = {
     },
 
     getAllBooksInStock: async () => {
-        const query = "SELECT * FROM library_stock";
+        const query = "SELECT * FROM novels";
         try {
 
             const [results] = await connection.execute(query)
@@ -52,7 +52,7 @@ const NovelModel = {
     },
 
     getBookFromStock: async (id) => {
-        const query = "SELECT * FROM library_stock WHERE bookID  =?";
+        const query = "SELECT * FROM novels WHERE bookID  =?";
         try {
 
             const [result] = await connection.execute(query, [id]);
@@ -66,7 +66,7 @@ const NovelModel = {
     },
 
     updateBookInStock: async (title, author, isbn, price, published_year, quantity, id) => {
-        const updateQuery = "UPDATE library_stock SET bookName =?, book_author =?, bookISBN =?, price =?, published_year =? , quantity =? WHERE bookID =?";
+        const updateQuery = "UPDATE novels SET bookName =?, book_author =?, bookISBN =?, price =?, published_year =? , quantity =? WHERE bookID =?";
         try {
 
             const [results] = await connection.execute(updateQuery, [title, author, isbn, price, published_year, quantity, id])
@@ -81,7 +81,7 @@ const NovelModel = {
     },
 
     deleteBookFromStock: async (id) => {
-        const deleteQuery = "DELETE FROM library_stock WHERE bookID = ?";
+        const deleteQuery = "DELETE FROM novels WHERE bookID = ?";
 
         try {
 
@@ -96,10 +96,10 @@ const NovelModel = {
 
     },
 
-    //functions to handle Book Lending
+    //functions to handle Book 
 
     findBookToLend: async (title, author) => {
-        const findBookQuery = "SELECT * FROM library_stock WHERE bookName =? AND book_author =? ";
+        const findBookQuery = "SELECT * FROM novels WHERE bookName =? AND book_author =? ";
 
         try {
             const [results] = await connection.execute(findBookQuery, [title, author]);
@@ -115,4 +115,4 @@ const NovelModel = {
 
 }
 
-module.exports = NovelModel;
+module.exports = NovelModel;Lending
